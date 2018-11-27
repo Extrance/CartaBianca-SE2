0743559690b27b76133b10b5c713c5bcc3e1c706
zioRock95 <scijar@gmail.com>
2018-11-27 18:15:02 +0100

    Added task funcs and tests

--- api.test.js
+++ api.test.js
@@ -108,6 +108,55 @@ describe('POST /groups/:id', async() => {
 
 //------------------------ TASKS ------------------------
 
+describe('GET /tasks/', async() => {
+  it('should return 200', async() => {
+    var t = await getTasks();
+    expect(t.status).toBe(200);
+  })
+})
+
+describe('GET /tasks/existingID', async() => {
+  it('should return 200', async() => {
+    var t = await getTaskByIdTest(4);
+    expect(t.status).toBe(200);
+  })
+})
+
+describe('GET /tasks/nonexistingID', async() => {
+  it('should return 404', async() => {
+    var t = await getTaskByIdTest(-1);
+    expect(t.status).toBe(404);
+  })
+})
+
+describe('POST /tasks/', async() => {
+  it('should return 400', async() => {
+    var t = await insTask();
+    expect(t).toBe(400);
+  })
+})
+
+describe('POST /tasks/', async() => {
+  it('should return 200', async() => {
+    var t = await insTask('provainsert2','prova_insert','42');
+    expect(t).toBe(200);
+  })
+})
+
+describe('POST /tasks/:id', async() => {
+  it('should return 200', async() => {
+    var t = await insertTaskById(4,'15');
+    expect(t).toBe(200);
+  })
+})
+
+describe('POST /tasks/:id', async() => {
+  it('should return 400', async() => {
+    var t = await insertTaskById();
+    expect(t).toBe(400);
+  })
+})
+
 //==================================== FUNCTIONS ====================================
 
 //-------------------- USERS + MAIN --------------------
@@ -206,3 +255,64 @@ async function insertGroupById(id,membro) {
 }
 
 //------------------------ TASKS ------------------------
+
+async function getTasks(){
+  var t = '<html><head></head><body><h1>Lista Tasks:</h1>';
+  var g = await query('SELECT * FROM "task";');
+  for (var i in g.rows){
+    //t += '<h2>'+g.rows[i].name+'</h2>';
+    t +=' '+'<a href="/tasks/'+g.rows[i].idtask+'"'+'>'+g.rows[i].name+'</a>'+'<br>';
+  }
+  if (logged){
+    t+='<br>';
+    t+='<h3>Crea Task</h3>';
+    t += '<form action="/tasks/" method="post"><input type ="text" name="name">Nome task<br /><input type ="text" name="desc">Descrizione task<br /><input type ="text" name="risp">Risposta<br /><button>Crea task!</button></form>';
+  }
+  return {status:200,text:t};
+}
+
+async function getTaskByIdTest(id) {
+  var t = {status:200,text:""}
+  try{
+    var t = await getTask(id);
+    return t;
+  }catch(e){
+    console.log('404');
+    t.status = 404;
+    t.text = "";
+    return t;
+  }
+}
+
+async function getTask(id){
+  var g = await query('SELECT * FROM "task" WHERE idtask = \''+id+'\';');
+  if(g.rows[0] != undefined) {
+    var t = '<html><head></head><body><h1>Task: '+g.rows[0].name+'</h1>';
+    t += '<h3>'+g.rows[0].description+'</h3>';
+
+    if (logged){
+      t += '<form action="/tasks/'+id+'" method="post">Rispondi:<input type ="number" name="ans"><button>Rispondi al task!</button></form>';
+    }
+    return {status:200,text:t};
+  }
+  return {status:404,text:""};
+}
+
+async function insTask(name,desc,risp) {
+  try{
+    await query('INSERT INTO "task" (name, description, answer) VALUES (\''+name+'\', \''+desc+'\', \''+risp+'\')');
+    return 200;
+  }catch(e){
+    return 400;
+  }
+}
+
+async function insertTaskById(id,ans) {
+  try{
+    var voto = 10;
+    await query('INSERT INTO "taskAw" (iduser, idtask, answer, mark) VALUES (\''+logId+'\', \''+id+'\', \''+ans+'\', \''+voto+'\')');
+    return 200;
+  }catch(e){
+    return 400;
+  }
+}
